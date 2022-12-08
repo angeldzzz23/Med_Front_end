adcb4607be5b0c5da55930e10ad540bd64199bc7
Arturo Rodriguez <112526110+ArtxRod17@users.noreply.github.com>
2022-12-08 13:19:24 -0800

    added add userMedicine function to network file. Allows for function to be called from another file to enter data to the server

--- med_tracker/med_tracker/Controller/AddingNewMedicine.swift
+++ med_tracker/med_tracker/Controller/AddingNewMedicine.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class AddingNewMedicine: UIViewController {
     //Variable to keep track of medicines (Object)
-    var medicines = [Medicine]()
     
     //Crete the variables for the adding of the medicine
     
@@ -29,43 +28,6 @@ class AddingNewMedicine: UIViewController {
         let medicineName = medName.text!
         let medicineAmount = medAmount.text!
         let medicineTime = medTime.text!
-        
-        let parameters = ["name" : medicineName, "type" : medicineAmount, "time": medicineTime]
-        let url = URL(string: "http://143.198.178.220:8000/medicine/create")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
-        
-        let task = URLSession.shared.dataTask(with: request) { (data,response,error) in
-            if let error = error {
-                //Handle error
-                print("Error sending request: \(error)")
-                return
-            }
-            //DATA RESPONSE FROM API POST
-            //ASIGNING IT TO A MEDICINE OBJECT
-           if let data = data {
-                //Parse the response data and create the medicine
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                    
-                    //Parse the JSON data and create the medicine object
-                    let medicine = Medicine(Medname: json["name"] as! String,MedCount: json["type"] as! String, medTime: json["time"] as! String)
-                    
-                    //Add medicine to the array
-                    self.medicines.append(medicine)
-                } catch {
-                    print("Error parsing repsonse: \(error)")
-                }
-            }
-            
-        }
-        
-        task.resume()
-        
-        
         
         
         
