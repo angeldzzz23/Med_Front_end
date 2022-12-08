4e61180be3bfa5992c2eaa6ead0467021ec29b7b
angel zambrano <angeldzambrano99@gmail.com>
2022-12-07 16:53:15 -0800

    added new things

---
+++ med_tracker/med_tracker/Networking.swift
@@ -0,0 +1,112 @@
+//
+//  Networking.swift
+//  med_tracker
+//
+//  Created by Angel Zambrano on 12/7/22.
+//
+
+import Foundation
+
+
+class API {
+    let shared = API()
+    
+    static func postRequest(username: String, password: String, completion: @escaping ([String: Any]?, Error?) -> Void) {
+
+        //declare parameter as a dictionary which contains string as key and value combination.
+        let parameters = ["email": username, "password": password]
+
+        //create the url with NSURL
+        let url = URL(string: "http://143.198.178.220:8000/api/login")!
+
+        //create the session object
+        let session = URLSession.shared
+
+        //now create the Request object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to data object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+            completion(nil, error)
+        }
+
+        //HTTP Headers
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request, completionHandler: { data, response, error in
+
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+
+            guard let data = data else {
+                completion(nil, NSError(domain: "dataNilError", code: -100001, userInfo: nil))
+                return
+            }
+
+            do {
+                //create json object from data
+                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
+                    completion(nil, NSError(domain: "invalidJSONTypeError", code: -100009, userInfo: nil))
+                    return
+                }
+                print(json)
+                completion(json, nil)
+            } catch let error {
+                print(error.localizedDescription)
+                completion(nil, error)
+            }
+        })
+
+        task.resume()
+    }
+    
+    
+   static func logIn(email: String, password: String, completion: @escaping (Result<LogIn?, Error>) -> Void) {
+            guard var url  = URLComponents(string: "http://143.198.178.220:8000/api/login") else {return}
+        
+        let json: [String: Any] = ["email": email, "password": password]
+
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        
+        var request = URLRequest(url: url.url!)
+        request.httpBody = jsonData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                do {
+                    let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+                    
+                    print(responseJSON)
+                    
+                    
+                    
+                    print(data)
+                    let decoder = JSONDecoder()
+                    let searchResponse = try decoder.decode(LogIn.self, from: data) // gets the artists
+                    
+
+                    completion(.success(searchResponse))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+
+        task.resume()
+        
+    }
+}
--- med_tracker/med_tracker/ViewController.swift
+++ med_tracker/med_tracker/ViewController.swift
@@ -7,13 +7,104 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct LogIn: Codable {
+    let jwt: String?
+}
 
+struct User: Codable {
+    let id: Int?
+    let name, email: String?
+}
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    
+    @IBOutlet weak var passwordtxtfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello")
+        
+//        UserDefaults.standard.set(true, forKey: "userLoggedIn")
+//        self.performSegue(withIdentifier: "loginToHome", sender: self)
+        
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set("Sheep787", forKey: "Password")
+
+        let email = defaults.string(forKey: "email")
+        let password =  defaults.string(forKey: "Password")
+        let lol =  defaults.string(forKey: "lol")
+        print("email", email)
+        print("password", password)
+        
+        if let email = email, let password = password {
+            //
+            API.logIn(email: email, password: password) { result in
+                switch result {
+                case .success(let logIn):
+                    DispatchQueue.main.async {
+                       
+                        self.performSegue(withIdentifier: "loginToHome", sender: self)
+                    }
+                    
+                case .failure(let error):
+                    print(error)
+                }
+            
+            }
+        }
+    
+        
+        
+        
+    }
+    
+    @IBAction func LoginButtonWasPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordtxtfield.text {
+            
+            
+            
+            
+            API.logIn(email: email, password: password) { result in
+                switch result {
+                case .success(let logIn):
+                    if logIn?.jwt == nil {
+                        print("password is incorrect")
+                    } else {
+                        let defaults = UserDefaults.standard
+                        defaults.set(email, forKey:"email")
+                        defaults.set(password, forKey:"Password")
+                        defaults.set(false, forKey: "lol")
+                        
+                        DispatchQueue.main.async {
+                           
+                            self.performSegue(withIdentifier: "loginToHome", sender: self)
+                        }
+                    }
+           
+                    
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        
+        
+        
+    }
+    
 
 
 }
