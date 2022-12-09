09efea71b407a59ec13e717f396a9e03eb1ec774
Arturo Rodriguez <112526110+ArtxRod17@users.noreply.github.com>
2022-12-09 01:11:11 -0800

    finally  finished to crating of a medicine. UI looks terrible but works. Next to focus on the delete portion

--- med_tracker/med_tracker/Controller/AddingNewMedicine.swift
+++ med_tracker/med_tracker/Controller/AddingNewMedicine.swift
@@ -9,23 +9,199 @@ import UIKit
 
 
 class AddingNewMedicine: UIViewController {
-    //Variable to keep track of medicines (Object)
-    
     //Crete the variables for the adding of the medicine
     
+    @IBOutlet weak var dateTF: UITextField!
     @IBOutlet weak var medName: UITextField!
-    @IBOutlet weak var medAmount: UITextField!
-    @IBOutlet weak var medTime: UITextField!
-    @IBOutlet weak var medDays: UITextField!
+    var typeChoice = ""
+    //Values above represent numbers to append to array
+    var daysChoice = [Int]()
+    
+    //---------------------Days of the week
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .time
+        datePicker.addTarget(self, action: #selector(dateChange(datePicker:)), for: UIControl.Event.valueChanged)
+        datePicker.frame.size = CGSize(width: 0, height: 300)
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = NSLocale(localeIdentifier: "en_GB") as Locale
+        
+        dateTF.inputView = datePicker
+        dateTF.text = formatDate(date: Date()) //todays date
+    }
+    
+    //TIME PICKER FUNCTION
+    @objc func dateChange(datePicker: UIDatePicker) {
+        dateTF.text = formatDate(date: datePicker.date)
+        
+    }
+    //FORMAT DATE
+    func formatDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: date)
+    }
+    
+    //Method to add to array of days click = add,click again = delete
+    
+    
+    //BUTTONS and assigning data
+    
+    @IBAction func mondayButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if(sender.isSelected == true) {
+            sender.backgroundColor = UIColor.black
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            daysChoice.append(1)
+        }
+        else {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            while daysChoice.contains(1) {
+                if let itemToRemoveIndex = daysChoice.firstIndex(of: 1) {
+                    daysChoice.remove(at: itemToRemoveIndex)
+                }
+            }
+           
+        }
+        
+        
     }
     
     
+    @IBAction func tuesdayButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if(sender.isSelected == true) {
+            sender.backgroundColor = UIColor.black
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            daysChoice.append(2)
+        }
+        else {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            while daysChoice.contains(2) {
+                if let itemToRemoveIndex = daysChoice.firstIndex(of: 2) {
+                    daysChoice.remove(at: itemToRemoveIndex)
+                }
+            }
+            
+        }
+    }
+    
+    
+    @IBAction func wednesdayButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if(sender.isSelected == true) {
+            sender.backgroundColor = UIColor.black
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            daysChoice.append(3)
+        }
+        else {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            while daysChoice.contains(3) {
+                if let itemToRemoveIndex = daysChoice.firstIndex(of: 3) {
+                    daysChoice.remove(at: itemToRemoveIndex)
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func thursdayButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if(sender.isSelected == true) {
+            sender.backgroundColor = UIColor.black
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            daysChoice.append(4)
+        }
+        else {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            while daysChoice.contains(4) {
+                if let itemToRemoveIndex = daysChoice.firstIndex(of: 4) {
+                    daysChoice.remove(at: itemToRemoveIndex)
+                }
+            }
+        }
+    }
+    
+    @IBAction func fridayButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if(sender.isSelected == true) {
+            sender.backgroundColor = UIColor.black
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            daysChoice.append(5)
+        }
+        else {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            while daysChoice.contains(5) {
+                if let itemToRemoveIndex = daysChoice.firstIndex(of: 5) {
+                    daysChoice.remove(at: itemToRemoveIndex)
+                }
+            }
+        }
+    }
+    
+    @IBAction func saturdayButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if(sender.isSelected == true) {
+            sender.backgroundColor = UIColor.black
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            daysChoice.append(6)
+        }
+        else {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            while daysChoice.contains(6) {
+                if let itemToRemoveIndex = daysChoice.firstIndex(of: 6) {
+                    daysChoice.remove(at: itemToRemoveIndex)
+                }
+            }
+        }
+    }
+    
+    @IBAction func sundayButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if(sender.isSelected == true) {
+            sender.backgroundColor = UIColor.black
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            daysChoice.append(7)
+        }
+        else {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.blue, for: .normal)
+            while daysChoice.contains(7) {
+                if let itemToRemoveIndex = daysChoice.firstIndex(of: 7) {
+                    daysChoice.remove(at: itemToRemoveIndex)
+                }
+            }
+        }
+    }
+    //METHOD TO CHANGE THE TIMES STRING
+    
+    
+    
+    
+    
+    //Should be last function to access global variabels to send to API
     @IBAction func addMedicineButtonTapped(_ sender: Any) {
+        //DEFINE THE INPUTS
+        let medicineName = medName.text!
+        let timeChosen = dateTF.text!
+        
         //Define URL
         guard let url = URL(string: "http://143.198.178.220:8000/medicine/create") else {
             return
@@ -35,11 +211,19 @@ class AddingNewMedicine: UIViewController {
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        if (daysChoice.count == 7){
+            typeChoice = "1"
+        }
+        else {
+            typeChoice = "2"
+        }
+        
         let body: [String: AnyHashable] = [
-            "name": "Example",
-            "type": "1",
-            "days": [1,2,3,4,5,6,7],
-            "time": "18:00"
+            "name": medicineName,
+            "type": typeChoice,
+            "days": daysChoice,
+            "time": timeChosen
         ]
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
         //Make request
